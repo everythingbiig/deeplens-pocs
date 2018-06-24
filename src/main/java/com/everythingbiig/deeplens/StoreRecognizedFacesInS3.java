e9dbbffdefed28de34a6cffdc85f0f7e5a4a70d0
Julio Santos <julio.r.santos.jr@gmail.com>
2018-06-24 09:11:25 -0400

    Fixed logging. Slimmed up the jar.

--- src/main/java/com/everythingbiig/deeplens/StoreRecognizedFacesInS3.java
+++ src/main/java/com/everythingbiig/deeplens/StoreRecognizedFacesInS3.java
@@ -4,19 +4,19 @@ package com.everythingbiig.deeplens;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class StoreRecognizedFacesInS3 {
-//    static final Logger logger = LogManager.getLogger(StoreRecognizedFacesInS3.class);
+    static final Logger logger = LogManager.getLogger(StoreRecognizedFacesInS3.class);
 
     private static final float RECOGNITION_THRESHOLD = 0.75f;
 
-//    private static final AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+    private static final AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
 
     private static final String OUTPUT_BUCKET = "com.everythingbiig.deeplens/faces";
 
-    private static final String OUTPUT_KEY = "recognized_face.jpg";
+    private static final String OUTPUT_KEY = "recognized_face.png";
 
 
     public static void main(String[] args) {
@@ -24,19 +24,19 @@ public class StoreRecognizedFacesInS3 {
     }
 
     public void handleEvent(FaceRecognitionEvent event, Context context) {
-        log("Got event: " + event);
+        logger.debug("Got event: " + event);
         if(event == null) {
-            log("Got empty result");
+            logger.debug("Got empty result");
         }
         if(event.getFace() > RECOGNITION_THRESHOLD) {
-            log("Result is above threshold: " + event.getFace() + " > RECOGNITION_THRESHOLD(" + RECOGNITION_THRESHOLD+")");
+            logger.debug("Result is above threshold: " + event.getFace() + " > RECOGNITION_THRESHOLD(" + RECOGNITION_THRESHOLD+")");
 //            s3Client.putObject(OUTPUT_BUCKET, OUTPUT_KEY, FILE);
-//            s3Client.copyObject("com.everythingbiig.deeplens/faces", "julioheadshot.png", OUTPUT_BUCKET, OUTPUT_KEY);
-            log("Stored face.");
+            s3Client.copyObject("com.everythingbiig.deeplens/faces", "julioheadshot.png", OUTPUT_BUCKET, OUTPUT_KEY);
+            logger.debug("Stored face.");
         }
 
     }
-    private static void log(String msg) {
-        System.out.println(msg);
-    }
+//    private static void log(String msg) {
+//        System.out.println(msg);
+//    }
 }
